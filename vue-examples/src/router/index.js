bb5003b33317999e99dfd74cfd5fc9c3d0fae98c
Nicolás Salas V <nikosalas@gmail.com>
2020-01-05 23:09:14 -0300

    Add examples for data and computed properties

--- vue-examples/src/router/index.js
+++ vue-examples/src/router/index.js
@@ -11,13 +11,15 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/data",
+    name: "data",
+    component: () => import(/* webpackChunkName: "about" */ "../views/Data.vue")
+  },
+  {
+    path: "/computed",
+    name: "computed",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "computed" */ "../views/Computed.vue")
   }
 ];
 
---
+++ vue-examples/src/views/Computed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>Computed property example</h1>
+    <div>
+      <input type="text" v-model="name" placeholder="Place your name here" />
+      <input
+        type="text"
+        v-model="lastName"
+        placeholder="Place your last name here"
+      />
+      <div>
+        {{ fullName }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: null,
+      lastName: null
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.name} ${this.lastName}`;
+    }
+  }
+};
+</script>
